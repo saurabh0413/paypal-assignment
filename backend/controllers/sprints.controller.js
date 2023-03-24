5216e20306fbfbb788e0e36b4dcaef5a8b624628
saurabh <saurabhfarkade83@gmail.com>
2023-03-24 18:56:42 +0530

    sprint-created

--- backend/controllers/sprints.controller.js
+++ backend/controllers/sprints.controller.js
@@ -1,7 +1,13 @@
+const { sprintModel } = require("../models/sprint.model");
 
+const sprintController = async (req, res) => {
+  const { userId, sprintName } = req.body;
+  const sprint = new sprintModel({
+    userId,
+    sprintName,
+  });
+  await sprint.save();
+  res.send(sprint);
+};
 
-const sprintController = async(req,res)=>{
-
-}
-
-module.exports = {sprintController}
+module.exports = { sprintController };
