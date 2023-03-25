6aeaf36a068c920a6aa6c7c862e5e3e0e62187fc
saurabh <saurabhfarkade83@gmail.com>
2023-03-25 13:54:07 +0530

    routes-added

--- backend/index.js
+++ backend/index.js
@@ -8,16 +8,7 @@ const { tasksRoute } = require("./routes/tasks.routes");
 const { updateTaskController } = require("./controllers/tasks.controller");
 
 const app = express();
-app.options("*", cors());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors())
 app.use(express.json());
 
 app.use("/", authRoute);
